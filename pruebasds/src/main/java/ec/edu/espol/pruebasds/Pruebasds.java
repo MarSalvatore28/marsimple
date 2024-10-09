9bd17a79b11ce21fe0390c1b3a6c934591552b38
MARLO VALLE <mcvalle@espol.edu.ec>
2024-10-08 20:53:55 -0500

    holaaaa
    ya hicimos algo se supone

---
+++ pruebasds/src/main/java/ec/edu/espol/pruebasds/Pruebasds.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.pruebasds;
+
+/**
+ *
+ * @author marsa
+ */
+public class Pruebasds {
+
+    public static void main(String[] args) {
+        System.out.println("hola yo soy la version original Mar Salvatore yo soy el mejor obvio");
+    }
+}
