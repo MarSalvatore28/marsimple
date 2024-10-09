71f4296a739f6ec47ae4155c77663e80eb856365
Daniela Reyes <152106996+dannreye@users.noreply.github.com>
2024-10-08 22:16:55 -0500

    dann
    ola

--- pruebasds/src/main/java/ec/edu/espol/pruebasds/Pruebasds.java
+++ pruebasds/src/main/java/ec/edu/espol/pruebasds/Pruebasds.java
@@ -13,5 +13,6 @@ public class Pruebasds {
     public static void main(String[] args) {
         System.out.println("hola ahora soy la persona A ");
         System.out.println("hola soy yo la persona b ");
+        System.out.println("ola como estas");
     }
 }
